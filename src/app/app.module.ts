83d3a7d3ef64018b8dcf3ed998f5eef0a274359b
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2020-04-05 01:10:03 -0300

    TemplateUrl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
- 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
